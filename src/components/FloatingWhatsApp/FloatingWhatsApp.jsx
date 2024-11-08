fcb23ecbddc052df98bc1c102c92e7f9a06f0726
AguileraNicolasJavier <aguileranicolasjavier@gmail.com>
2024-11-08 13:41:28 +0000

    Descripción de los cambios realizados

--- src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
+++ src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
@@ -1,11 +1,67 @@
-import Whatsapp_icon from '/public/img/Floating_whatsapp/whatsapp.png';
+import React, { useState, useEffect, useRef } from "react";
+import "./_floatingWhatsApp.scss";
+import Whatsapp_icon from "/public/img/Floating_whatsapp/whatsapp.png";
+import contactIcon from "/public/img/Floating_whatsapp/contact-icon.png";
 
 function FloatingWhatsApp() {
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const dropdownRef = useRef(null);
+    const iconRef = useRef(null);
+
+    // Función para cambiar el estado de visibilidad de los íconos
+    const toggleDropdown = () => {
+        setIsDropdownVisible(prevState => !prevState);
+    };
+
+    // Cerrar el dropdown si se hace clic fuera de él
+    const handleClickOutside = (event) => {
+        if (
+            dropdownRef.current && !dropdownRef.current.contains(event.target) && 
+            iconRef.current && !iconRef.current.contains(event.target)
+        ) {
+            setIsDropdownVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className="floating-whatsapp">
-            <a href="https://wa.me/543513297292" target="_blank" rel="noopener noreferrer">
+            {/* Icono de WhatsApp que al hacer clic activa el dropdown */}
+            <div 
+                className="whatsapp-icon" 
+                onClick={toggleDropdown} 
+                ref={iconRef}
+            >
                 <img src={Whatsapp_icon} alt="WhatsApp" />
-            </a>
+            </div>
+
+            {/* Los íconos desplegados se muestran solo si isDropdownVisible es true */}
+            <div 
+                className={`dropdown ${isDropdownVisible ? 'show' : ''}`} 
+                ref={dropdownRef}
+            >
+                <a 
+                    href="https://wa.me/1234567890" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="dropdown-item"
+                >
+                    <img src={Whatsapp_icon} alt="Contacto por WhatsApp" />
+                </a>
+                <a 
+                    href="/formulario-contacto"
+                    className="dropdown-item"
+                >
+                    <img src={contactIcon} alt="Formulario de Contacto" />
+                </a>
+            </div>
         </div>
     );
 }
