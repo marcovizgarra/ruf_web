d8b6d577619fb2226a792d4617cd919c343fa5cb
AguileraNicolasJavier <aguileranicolasjavier@gmail.com>
2024-11-08 12:57:40 +0000

    public/img/Floating_whatsapp/whatsapp.png

--- src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
+++ src/components/FloatingWhatsApp/FloatingWhatsApp.jsx
@@ -1,11 +1,13 @@
-const FloatingWhatsApp = () => {
+import Whatsapp_icon from '/public/img/Floating_whatsapp/whatsapp.png';
+
+function FloatingWhatsApp() {
     return (
         <div className="floating-whatsapp">
             <a href="https://wa.me/543513297292" target="_blank" rel="noopener noreferrer">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/WhatsApp_icon.png" alt="WhatsApp" />
+                <img src={Whatsapp_icon} alt="WhatsApp" />
             </a>
         </div>
     );
-};
+}
 
 export default FloatingWhatsApp;
